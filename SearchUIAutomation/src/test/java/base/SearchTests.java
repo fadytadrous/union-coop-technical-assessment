1871047dacd43f904f80203fafe0cac2f152d1bf
fadytadrous <43914477+fadytadrous@users.noreply.github.com>
2024-02-09 14:37:34 +0200

    added documentation

--- SearchUIAutomation/src/test/java/base/SearchTests.java
+++ SearchUIAutomation/src/test/java/base/SearchTests.java
@@ -1,3 +1,8 @@
+/*
+ * This class contains test methods for testing different functionalities related to search.
+ * It extends the Base class to inherit setup and teardown methods for WebDriver initialization and cleanup.
+ */
+
 package base;
 
 import org.testng.annotations.Test;
@@ -7,6 +12,10 @@ import static org.testng.Assert.*;
 
 public class SearchTests extends base {
 
+    /**
+     * Test method to verify successful search using the click action.
+     * It sets the search input, clicks the search button, and verifies if the expected result is found.
+     */
     @Test
     public void testSuccessfulSearchUsingClick(){
         String searchInput = "union coop";
@@ -18,6 +27,10 @@ public class SearchTests extends base {
                 "Search results using Click don't match with input");
     }
 
+    /**
+     * Test method to verify successful search using the Enter button.
+     * It sets the search input, presses the Enter key, and verifies if the expected result is found.
+     */
     @Test
     public void testSuccessfulSearchUsingEnter(){
         String searchInput = "union coop";
@@ -26,9 +39,13 @@ public class SearchTests extends base {
         SearchResultsPage searchResultsPage = searchPage.clickSearch();
 
         assertTrue(searchResultsPage.isResultFound(searchInput),
-                "Search results using Enter Button  don't match with input");
+                "Search results using Enter Button don't match with input");
     }
 
+    /**
+     * Test method to verify the functionality of the Clear button.
+     * It sets the search input, clicks the Clear button, and verifies if the search input field is empty.
+     */
     @Test
     public void testClearBtn(){
         String searchInput = "union coop";
@@ -37,9 +54,13 @@ public class SearchTests extends base {
         searchPage.clearBtn.click();
 
         assertTrue(searchPage.searchInput.getText().isEmpty(),
-                "Clear Button  isn't working properly");
+                "Clear Button isn't working properly");
     }
 
+    /**
+     * Test method to verify the functionality of the Feeling Lucky button.
+     * It sets the search input, clicks the Feeling Lucky button, and verifies if the current URL contains the search input.
+     */
     @Test
     public void testFeelingLuckyBtn(){
         String searchInput = "unioncoop";
--- SearchUIAutomation/src/test/java/base/base.java
+++ SearchUIAutomation/src/test/java/base/base.java
@@ -1,14 +1,18 @@
+/*
+ * This class serves as a base class for test classes in the test suite.
+ * It provides setup and teardown methods for WebDriver initialization and cleanup.
+ */
 package base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
+
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
+
 import org.testng.annotations.BeforeMethod;
 import pages.SearchPage;
-import pages.SearchResultsPage;
+
 
 public class base {
 
@@ -16,6 +20,12 @@ public class base {
 
     protected SearchPage searchPage;
 
+    /**
+     * Method to set up the WebDriver instance and navigate to the application URL before each test method.
+     * It initializes the WebDriver using WebDriverManager for Chrome, navigates to the application URL,
+     * maximizes the browser window, and initializes the SearchPage object.
+     * For the sake of making all tests in one run under searchTest Class BeforeMethod was used
+     */
     @BeforeMethod
     public void setup(){
         // Setup WebDriver using WebDriverManager for Chrome
@@ -26,12 +36,14 @@ public class base {
         driver.get("https://www.google.com/");
         driver.manage().window().maximize();
 
+        // Initialize the SearchPage object
         searchPage = new SearchPage(driver);
     }
 
     /**
      * Method to tear down the WebDriver instance after executing tests.
      * Quits the WebDriver session.
+     * For the sake of making all tests in one run under searchTest Class AfterMethod was used
      */
     @AfterMethod
     public void tearDown(){
