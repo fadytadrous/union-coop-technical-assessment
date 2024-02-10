f799249f24a27b79003b43c0bbaea47229ab0452
fadytadrous <43914477+fadytadrous@users.noreply.github.com>
2024-02-10 17:18:40 +0200

    removed unnecessary libs

--- SearchUIAutomation/src/main/java/pages/SearchResultsPage.java
+++ SearchUIAutomation/src/main/java/pages/SearchResultsPage.java
@@ -1,13 +1,6 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-import java.util.List;
 
 public class SearchResultsPage {
     private WebDriver driver;  // WebDriver instance to interact with the browser
